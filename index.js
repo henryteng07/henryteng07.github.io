38613b0e591d9c5f4d0ef93eb3e2a8f43a84c4cd
henryteng07 <61112534+henryteng07@users.noreply.github.com>
2020-09-25 03:02:15 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,76 @@
-// //Get the button:
-// mybutton = document.getElementById("myBtn");
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const app = express();
+const https = require("https");
 
-// // When the user scrolls down 20px from the top of the document, show the button
-// window.onscroll = function() {scrollFunction()};
+app.use(express.static(__dirname +"/public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
-// function scrollFunction() {
-//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//     mybutton.style.display = "block";
-//   } else {
-//     mybutton.style.display = "none";
-//   }
-// }
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/public/home.html");
+})
 
-// // When the user clicks on the button, scroll to the top of the document
-// function topFunction() {
-//   document.body.scrollTop = 0; // For Safari
-//   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-// }
+app.post("/failure", function(req, res) {
+    res.redirect("/");
+})
+
+app.post("/", function(req, res) {
+    const firstname = req.body.fname;
+    const email = req.body.eml;
+    const number = req.body.num;
+    const userSelection = req.body.usermultiselect;
+    const additionalText = req.body.txt;
+
+    console.log(firstname, email, number, userSelection, additionalText);
+
+    // TODO
+    const data = {
+        members: [
+        {
+            email_address: email,
+            status: "subscribed",
+            merge_fields: { 
+                FNAME: firstname,
+                PHONE: number,
+                USERCHC: userSelection,
+                USERCMT: additionalText
+            }
+
+        }
+    ]
+    }
+
+    const jsonData = JSON.stringify(data);
+    const url = "https://us17.api.mailchimp.com/3.0/lists/e11263e79b"
+
+    const option = {
+        method: "POST",
+        auth: "henry:a0c96c8fa4491dba221c29969accaa5c-us17"
+    }
+
+    const request = https.request(url, option, function(response){
+        response.on("data", function(data) {
+
+            if (response.statusCode === 200) {
+                res.sendFile(__dirname+ "/success.html");
+            } else {
+                res.sendFile(__dirname+ "/failure.html");
+            }
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+})
+app.listen(3000, function() {
+    console.log("Server is running on port 3000");
+})
+
+
+// api Key
+// a0c96c8fa4491dba221c29969accaa5c-us17
+
+//audience id
+// e11263e79b
